d8626e04a2974c43df5b91688ddadcdbfa7ba48e
Daniel Harfleet <>
2020-10-14 16:43:53 +0100

    Updates

---
+++ precache-manifest.9881835b80635d40e0aa7c36c2106a41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26392db0ca0d242c13817a30753bb58d",
+    "url": "/content/index.html"
+  },
+  {
+    "revision": "6d478c2bf3d8081ea9c9",
+    "url": "/content/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7cdfc7385ae12a53241c",
+    "url": "/content/static/js/2.3b967ee5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/content/static/js/2.3b967ee5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d478c2bf3d8081ea9c9",
+    "url": "/content/static/js/main.74ae1fbb.chunk.js"
+  },
+  {
+    "revision": "c5ce3796dca404b81ac7",
+    "url": "/content/static/js/runtime-main.44ec8d56.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/content/static/media/logo.5d5d9eef.svg"
+  }
+]);
